dcbede6508a565fbf3ad99b204d6b44df6ecab6c
Ricardo Condori <ricardo.condori@blimop.tech>
2025-01-26 02:08:05 -0300

    init

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+     experimental: {
+         appDir: true
+     },
+     output: 'export', 
+};
  
 export default withNextIntl(nextConfig);
